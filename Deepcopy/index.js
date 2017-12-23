d552df368374517206c829fa50d20252a66d501c
hizhou <m15609286458@163.com>
2017-12-23 12:12:26 +0800

    es5 es6中的方法，基本都是首层深拷贝

--- Deepcopy/index.js
+++ Deepcopy/index.js
@@ -81,17 +81,49 @@ console.log(arr1);
 console.log(arr1Copy);
 
 //fourth is es6 methods
-var Object5=Object.create({},{});
+console.log("Object.assign方法实现首层深拷贝，往下是浅拷贝");
+console.log("首先，Object.assign方法，不会拷贝继承属性和不可枚举属性，可枚举属性和Symbol会被继承");
+var Object5=Object.create({name:"baba"},{    // name 是个继承属性
+    year: {
+        value: 2                             // year 是个不可枚举属性
+    },
+    hobby:{                                  // hobby 是个自身可枚举属性
+        value:"soccer",                      // create时必须使用value属性
+        enumerable:true,
+        writable: true
+    }
+});
 
+var Object6=Object.assign({},Object5);
+console.log(Object5);
+console.log(Object6);
 
+console.log("拷贝修改前");
+var obj1 = { a: 1 };
+var obj2={b:{c:0}};
+var obj3={[Symbol("lovers")]:"hehehe"};
+var copy1 = Object.assign({name:"baba"}, obj1,obj2,obj3);
+var copy2 = Object.assign({},copy1);
+console.log(copy1); 
+console.log(copy2);
+console.log("拷贝修改后");
+copy1.a=3;
+copy1.b.c=4;
+copy2.name="lala";
+console.log(copy1); 
+console.log(copy2);
 
+//Object.create({原型},{新来的家伙})
+//Object.assign({原对象},{对象1}{对象2}{对象3})
 
-
-
-
-
-
-
-
-
-
+console.log("运算符进行拷贝。首层深拷贝，往下面是浅拷贝");
+var Object7 = {a:1,b:{c:2}};
+var Object8 = {...Object7};
+console.log('拷贝前');
+console.log(Object7);
+console.log(Object8);
+console.log("拷贝后");
+Object7.a=30;
+Object7.b.c=100;
+console.log(Object7);
+console.log(Object8);
